b8b12ce10e4149ef57af383c621207afff06738c
ATMECS\Prashanth.Chinala <prashanth.chinala@atmecs.com>
2022-04-29 15:48:07 +0530

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express=require("express")
 const app=express();
+const PORT= process.env.PORT || 3000
 
 const communicationRouter =require('./routes/communications');
 app.use('/communications',communicationRouter)
 
-app.listen(3000)
-console.log("server running in 3000")
+app.listen(PORT)
+console.log(`Listening at port ${PORT}` )
